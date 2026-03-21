1b2e48401872b45e9bbaff9cebfcfb668983041c
avabellaw <70448139+avabellaw@users.noreply.github.com>
2026-03-21 11:16:28 +0000

    Use global vars for container info and improve logging

--- update-containers.sh
+++ update-containers.sh
@@ -11,48 +11,38 @@ DRY_RUN=true
 
 function log() {
     LEVEL=${2:-Info}
-    echo "[$LEVEL] $1"
+    echo -e "[$LEVEL] $1"
 }
 
-if [ $DRY_RUN == true ]; then log "DRY RUN - Updates won't be performed"; fi
-
-# Snippet taken from "sshane" and modified - https://forum.proxmox.com/threads/update-all-lxc-with-one-simple-script.58729/
-
-containers=$(pct list | tail -n +2 | cut -f1 -d' ')
-
 function update_container() {
-  local container=$1
-  local hostname=`pct config $container | grep hostname`
 
-  log "Updating #$container - $hostname"
+  log "Updating $curr_container_str"
   
   if [ $DRY_RUN != true ]; then
-    pct exec $container -- bash -c "apt update && apt upgrade -y";
+    pct exec $curr_container_id -- bash -c "apt update && apt upgrade -y";
   fi
 }
 
 function start_container() {
-    local container=$1
-    log "Starting #$container";
+    log "Starting $curr_container_str";
 
     if [ $DRY_RUN==true ]; then 
         return 0;
     fi
 
-    pct start $container
+    pct start $curr_container_id
     log "Sleeping for 5s"
     sleep 5
 }
 
 function shutdown_container() {
-    local container=$1
-    log "Shutting down #$container";
+    log "Shutting down $curr_container_str";
 
     if [ $DRY_RUN==true ]; then 
         return 0;
     fi
 
-    pct shutdown $container &
+    pct shutdown $curr_container_id &
 }
 
 function is_excluded() {
@@ -66,22 +56,29 @@ function is_excluded() {
     return 0
 }
 
-for container in $containers
-do
-  is_excluded $container
+if [ $DRY_RUN == true ]; then log "DRY RUN - Updates won't be performed"; fi
+
+containers=$(pct list | tail -n +2)
+
+while IFS=$' ' read -r id _ hostname; do
+  curr_container_id=$id;
+  curr_container_hostname=$hostname;
+  curr_container_str="\t#$curr_container_id\t $curr_container_hostname"
+  
+  is_excluded $curr_container_id
 
   if [ $? -eq 1 ]; then 
-    log "Excluding $container";
-    sleep 2;
+    log "Excluding $curr_container_str";
     continue
   fi
 
-  status=`pct status $container`
+  status=`pct status $curr_container_id`
   if [[ "$status" == "status: stopped" && ("$REACH"=="all" || "$REACH"=="stopped") ]]; then
-    start_container $container
-    update_container $container
-    shutdown_container $container
+    start_container
+    update_container
+    shutdown_container
   elif [[ "$status" == "status: running" && ("$REACH"=="all" || "$REACH"=="running") ]]; then
-    update_container $container
+    update_container
   fi
-done; wait
+done <<< $containers
+wait
